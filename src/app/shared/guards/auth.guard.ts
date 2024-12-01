285384fe2e111cdd16e90f0d005807ad04433b68
Dzheerro <mark.bulavin2005@gmail.com>
2024-12-01 17:17:58 +0200

    added global styles, implemented signup page and logic for it with usign uuid, disabled ssr

---
+++ src/app/shared/guards/auth.guard.ts
@@ -0,0 +1,14 @@
+import { inject } from '@angular/core';
+import { CanActivateFn, Router } from '@angular/router';
+import { UserService } from '../services/user.service';
+
+export const AuthGuard: CanActivateFn = () => {
+  const router: Router = inject(Router);
+  const userService: UserService = inject(UserService);
+
+  if (!userService.isLoggedIn()) {
+    router.navigateByUrl('/sign-up');
+    return false;
+  }
+  return true;
+};
